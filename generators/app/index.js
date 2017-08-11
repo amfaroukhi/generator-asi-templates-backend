009d9076c5c732cb1416165736ec7bd6ff9b4c02
Amien FAROUKHI <amine.faroukhi@gmail.com>
2017-08-11 11:03:28 +0200

    Préparation version basique (based on yeoman generator-generator)

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,129 @@
 'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+//Require dependencies
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
 
-module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the super-duper ' + chalk.red('generator-asi-templates-backend') + ' generator!'
-    ));
+module.exports = yeoman.generators.Base.extend({
+//Configurations will be loaded here.
+//Ask for user input
+  prompting: function () {
+    var done = this.async();
+    this.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'asi-templates-backend question',
+      //Defaults to the project's folder name if the input is skipped
+      default: this.appname
+    }, function (answers) {
+      this.props = answers
+      this.log(answers.name);
+      done();
+    }.bind(this));
+  },
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+  //Writing Logic here
+  writing: {
+    //Copy the configuration files
+    config: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'), {
+          name: this.props.name
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json'), {
+          name: this.props.name
+        }
+      );
+      this.fs.copy(
+        this.templatePath('bowerrc'),
+        this.destinationPath('.bowerrc')
+      );
+    },
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+    //Copy application files
+    app: function () {
+      //Server file
+      this.fs.copyTpl(
+        this.templatePath('_server.js'),
+        this.destinationPath('server.js'),
+        this.destinationPath('/views/index.ejs'), {
+          name: this.props.name
+        }
+      );
+      /////Routes
+      this.fs.copy(
+        this.templatePath('_routes/_all.js'),
+        this.destinationPath('routes/all.js'));
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+      // Model
+      this.fs.copy(
+        this.templatePath('_model/_todo.js'),
+        this.destinationPath('model/todo.js'));
 
-  install() {
+      // Views
+      this.fs.copyTpl(
+        this.templatePath('_views/_index.ejs'),
+        this.destinationPath('/views/index.ejs'), {
+          name: this.props.name
+        }
+      );
+
+      // Public/
+      this.fs.copy(
+        this.templatePath('_public/_css/_app.css'),
+        this.destinationPath('public/css/app.css')
+      );
+      this.fs.copy(
+        this.templatePath('_public/_js/_app.js'),
+        this.destinationPath('public/js/app.js')
+      );
+    }
+  },
+
+  //Install Dependencies
+  install: function() {
     this.installDependencies();
   }
-};
+});
+
+
+// 'use strict';
+// const Generator = require('yeoman-generator');
+// const chalk = require('chalk');
+// const yosay = require('yosay');
+//
+// module.exports = class extends Generator {
+//   prompting() {
+//     // Have Yeoman greet the user.
+//     this.log(yosay(
+//       'Welcome to the super-duper ' + chalk.red('generator-asi-templates-backend') + ' generator!'
+//     ));
+//
+//     const prompts = [{
+//       type: 'confirm',
+//       name: 'someAnswer',
+//       message: 'Would you like to enable this option?',
+//       default: true
+//     }];
+//
+//     return this.prompt(prompts).then(props => {
+//       // To access props later use this.props.someAnswer;
+//       this.props = props;
+//     });
+//   }
+//
+//   writing() {
+//     this.fs.copy(
+//       this.templatePath('dummyfile.txt'),
+//       this.destinationPath('dummyfile.txt')
+//     );
+//   }
+//
+//   install() {
+//     this.installDependencies();
+//   }
+// };
